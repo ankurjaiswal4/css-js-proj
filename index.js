2ffa0e86555c61a3689d8b10e45c95c231464026
Ankur Jaiswal <ankurjaiswalcse@gmail.com>
2020-01-01 22:40:09 +0530

    first commit - js

---
+++ index.js
@@ -0,0 +1,77 @@
+//What are Javascript types?
+/*
+Number
+String
+Boolean
+undefined
+null
+Function
+Object
+Array
+*/
+
+//How to find the type array in Javascript?
+/*
+Array.isArray([1, 2, 3]);  // true
+Array.isArray({foo: 123}); // false
+Array.isArray('foobar');   // false
+Array.isArray(undefined);  // false
+*/
+
+//How to find max digit in number of length n
+
+//How to find array in Javascript
+
+//In how many ways we can create objects in Javascript
+
+//What is negative infinity?
+/* Negative infinity is a number which you can get in JavaScript by dividing negative number by zero. */
+
+//What is debouncing in Javascript? 
+/* Excute after some time only no matter many times you click the button....We use it because certain operation takes some time to execute */
+
+//What are pseudo elements and classes in css
+
+//Spread vs Rest Operator
+
+//What is flexbox in CSS3
+
+//What is the precedence of css, inline, internal, external
+
+//Difference between .on('click') vs .click()
+
+//regular expression javascript
+
+//what is transclude in angular js directive
+
+//how to place content in html and keep it hidden //template in html5 //how to read template in javascript
+
+//What are server-sent events(sse)/ EventSource?
+
+//what is event bubbling & event delegation?
+
+//What is precedence in class  //<p class="red" [class]="green" [ngClass]="'yellowBkg'">
+
+//align div in the middle of the page
+
+//what is css grid
+
+//Ajax, Reverse Ajax, and WebSockets && Comet, Piggyback
+
+/*
+//Progress Bar at top when scroll
+<script>
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+</script> */
+
+// prototype is a property of a Function object. It is the prototype of objects constructed by that function.
+//__proto__ is internal property of an object, pointing to its prototype.
+//Current standards provide an equivalent Object.getPrototypeOf(O) method, though de facto standard __proto__ is quicker.
